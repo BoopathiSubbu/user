fd0a81a09f2dd25cac277f2ac422d97d3c323324
Subbu lakshmi <143779397+BoopathiSubbu@users.noreply.github.com>
2023-12-23 13:18:18 +0300

    Delete annauniv directory

--- annauniv/src/main/java/com/annauniv/dao/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.annauniv.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.annauniv.model.StudentDetails;
-
-public interface StudentRepository extends JpaRepository<StudentDetails, Integer>{
-
-}
